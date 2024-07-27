1ac9e1fe7033c25fe64f62d14ebee565aab953de
wuxs <317009160@qq.com>
2024-07-27 14:46:16 +0800

    feat: 移除质量中的环境变量

--- build/vite.analy.config.ts
+++ build/vite.analy.config.ts
@@ -1,6 +1,5 @@
 
 import {defineConfig, loadEnv} from 'vite'
-import vue from '@vitejs/plugin-vue'
 import { analyzer } from 'vite-bundle-analyzer'
 import {alias} from "./utils";
 import {createVitePlugins} from "./plugins.ts";
