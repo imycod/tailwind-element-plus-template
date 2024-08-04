05c945aa3cd5060d56ed51d965a473b8d79dc8dc
wuxs <317009160@qq.com>
2024-08-05 07:24:22 +0800

    feat：table & search

--- src/views/userList/index.vue
+++ src/views/userList/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import {useTable} from "@/hooks/useTable.ts";
-import {getUsers} from "@/apis/user.ts";
+import { useTable } from "@/hooks/useTable.ts";
+import { getUsers } from "@/apis/user.ts";
 
 const state = reactive({
   loading: false,
@@ -19,42 +19,53 @@ const state = reactive({
   pageList: getUsers,// someapi
   descs: ['create_time'],
 })
-const {getDataList, currentChangeHandle, sizeChangeHandle, downBlobFile, tableStyle} = useTable(state)
+const { getDataList, currentChangeHandle, sizeChangeHandle, downBlobFile, tableStyle } = useTable(state)
+const { getSchema, form } = useSearch()
+const genderOptions = ref([])
+const schema = computed(() => {
+  let el = [{ type: 'input', name: 'username' }, { type: 'select', name: 'gender', options: genderOptions.value }, { type: 'input', name: 'cangku' }, { type: 'date', name: 'createTime' }, { type: 'select', name: 'who', options: [] }]
+  return el.map(e => {
+    console.log(getSchema(e));
+    return getSchema(e)[0]
+  })
+})
+function callback(type) {
+  if (type === 'reset') {
+    form.value = {}
+    return
+  }
+  console.log(form.value);
+
+}
+
+onMounted(() => {
+  setTimeout(() => {
+    genderOptions.value = [
+      { label: '男', value: '1' },
+      { label: '女', value: '2' }
+    ]
+  }, 1000);
+})
 
 function handleSelectionChange(objs: { roleId: string }[]) {
 }
-
-// 清空搜索条件
-const resetQuery = () => {
-
-};
 </script>
 
 <template>
   <div>
-    <el-table
-        :data="state.dataList"
-        v-loading="state.loading"
-        @selection-change="handleSelectionChange"
-        border
-        class="item-custom-table"
-        max-height="80vh"
-    >
-      <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column prop="id" label="角色ID" width="100"></el-table-column>
-      <el-table-column prop="name" label="角色名称"></el-table-column>
-      <el-table-column prop="role" label="角色编码"></el-table-column>
-    </el-table>
-    <el-pagination
-        v-if="state.pagination.total"
-        v-model:current-page="state.pagination.current"
-        v-model:page-size="state.pagination.size"
-        :page-sizes="[10, 30, 50, 100]"
-        layout="sizes, prev, pager, next"
-        :total="state.pagination.total"
-        @size-change="sizeChangeHandle"
-        @current-change="currentChangeHandle"
-    />
+    <search :schema="schema" v-model="form" :callback="callback"></search>
+    <div class="dark:border-b pb-2 dark:border-gray-800 dark:border-l dark:border-r dark:rounded-lg">
+      <el-table :data="state.dataList" v-loading="state.loading" @selection-change="handleSelectionChange" border
+        class="item-custom-table" max-height="80vh">
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="id" label="角色ID" width="100"></el-table-column>
+        <el-table-column prop="name" label="角色名称"></el-table-column>
+        <el-table-column prop="role" label="角色编码"></el-table-column>
+      </el-table>
+      <el-pagination v-if="state.pagination.total" v-model:current-page="state.pagination.current"
+        v-model:page-size="state.pagination.size" :page-sizes="[10, 30, 50, 100]" layout="sizes, prev, pager, next"
+        :total="state.pagination.total" @size-change="sizeChangeHandle" @current-change="currentChangeHandle" />
+    </div>
   </div>
 </template>
 
